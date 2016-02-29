8d0a024426c1c9de4181ecab563dce759206b404
LinkofOrigin <jesselink333@gmail.com>
2016-02-29 14:38:08 -0700

    SQL

--- assets/jrupe.sql
+++ assets/jrupe.sql
@@ -2,13 +2,14 @@ create table users (
 	id int not null auto_increment primary key,
 	username varchar(64) not null,
 	password varchar(64) not null,
-	console varchar(3) not null,
+	console varchar(4) not null,
 	about varchar(256)
 );
 
 create table events (
 	id int not null auto_increment primary key,
 	sherpa int not null,
+	console varchar(4) not null,
 	activity int not null,
 	start datetime not null,
 	other varchar(512)
