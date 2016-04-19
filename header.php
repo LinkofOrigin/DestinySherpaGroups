f893a4b7a8de3e06524206d6ab237a69784d15c9
Jesse <jesselink333@gmail.com>
2016-04-18 21:24:24 -0600

    Styling for active page buttons in header bar

--- header.php
+++ header.php
@@ -3,17 +3,20 @@ require_once "Dao.php";
 $dao = new Dao();
 $row = $dao->getLogin();
 
+$accountActive = $here === "account.php" ? " activePage" : "";
+$viewActive = $here === "view.php" ? " activePage" : "";
+$createActive = $here === "create.php" ? " activePage" : "";
 ?>
 
 <header>
     <a id="headerTitle" href="index.php">Destiny<br>Sherpa Groups</a>
     <?php
-        if($row) {
-            echo "<a href='logout.php?redirect={$here}'><button id=\"logout\" class='headerButton'>Logout</button></a>";
-            echo "<a href='account.php'><button class='headerButton'>Account</button></a>";
-            echo "<a href='view.php'><buttton class='headerButton'>My Events</buttton></a>";
-	        echo "<a href='create.php'><button id='createButton' class='headerButton'>Create New Event</button></a>";
-        } else {
+        if($row) { ?>
+            <a href='logout.php?redirect=<?php echo $here; ?>'><button id="logout" class='headerButton'>Logout</button></a>
+            <a href='account.php'><button class='headerButton<?php echo $accountActive; ?>'>Account</button></a>
+            <a href='view.php'><button class='headerButton<?php echo $viewActive; ?>'>My Events</button></a>
+	        <a href='create.php'><button id='createButton' class='headerButton<?php echo $createActive; ?>'>Create New Event</button></a>
+        <?php } else {
             echo "<button id=\"login\" class='headerButton'>Login</button>";
         }
     ?>
